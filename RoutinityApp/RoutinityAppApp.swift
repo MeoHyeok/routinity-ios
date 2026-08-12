ea2422e87965a7a3cc107e76ab7eeb75301135bb
meohyeok <soonjang7748@gmail.com>
2026-08-12 17:41:48 +0900

    Initial Commit

---
+++ RoutinityApp/RoutinityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutinityAppApp.swift
+//  RoutinityApp
+//
+//  Created by 머혁 on 8/12/26.
+//
+
+import SwiftUI
+
+@main
+struct RoutinityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
